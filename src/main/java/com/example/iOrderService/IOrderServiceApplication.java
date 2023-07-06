5c1f752aa67f029785c3dbc99d41f41794c50db8
sqxsss <1514713140@qq.com>
2023-07-05 21:39:26 -0400

    Initial commit

---
+++ src/main/java/com/example/iOrderService/IOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.iOrderService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/iOrderService/IOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iOrderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

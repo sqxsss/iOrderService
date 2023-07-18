0d2c7f0d1d9095ce9c8cb7c8409da6160214ab38
sqxsss <1514713140@qq.com>
2023-07-17 21:45:02 -0400

    add OrderController test config

--- src/test/java/com/example/iOrderService/controller/OrderControllerTest.java
+++ src/test/java/com/example/iOrderService/controller/OrderControllerTest.java
@@ -1,15 +1,41 @@
 package com.example.iOrderService.controller;
 
 import com.example.iOrderService.OrderServiceConfigTest;
+import com.example.iOrderService.repository.OrderRepository;
+import com.example.iOrderService.service.OrderService;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @EnableConfigurationProperties
 @AutoConfigureMockMvc
 @ContextConfiguration(classes = {OrderServiceConfigTest.class})
 public class OrderControllerTest {
+    @Autowired
+    private OrderService orderService;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private MockMvc mockMvc;
+
+    static WireMockExtension wireMockExtension = WireMockExtension.newInstance()
+            .options(WireMockConfiguration.wireMockConfig().port(8053))
+            .build();
+
+    @Test
+    public void testPlaceOrderDoPaymentSuccess(){
+        // place order
+        // Get Order from DB by orderId
+        // Verify OrderResponse
+    }
 }

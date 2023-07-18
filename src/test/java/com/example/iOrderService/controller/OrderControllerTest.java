2ae16615367ca2bf15dacfbe172b3ab9753ab626
sqxsss <1514713140@qq.com>
2023-07-17 21:03:04 -0400

    add OrderController test supplier

---
+++ src/test/java/com/example/iOrderService/controller/OrderControllerTest.java
@@ -0,0 +1,15 @@
+package com.example.iOrderService.controller;
+
+import com.example.iOrderService.OrderServiceConfigTest;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@EnableConfigurationProperties
+@AutoConfigureMockMvc
+@ContextConfiguration(classes = {OrderServiceConfigTest.class})
+public class OrderControllerTest {
+
+}
